3def74c5f957814416e45d9d53e73ee8bb1669a3
Denis Nikitin <den221001@gmail.com>
2025-06-10 23:37:18 +0300

    feat: use mutex for one process of program

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.Threading;
 
 namespace Golden_votes;
 
@@ -8,9 +9,25 @@ class Program
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
+    private static readonly Mutex AppMutex = new Mutex(true, "Global\\GoldenVotes.mu");
+
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // Check if another instance is already running
+        if (!AppMutex.WaitOne(TimeSpan.Zero, true))
+            return;
+
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        finally
+        {
+            AppMutex.ReleaseMutex();
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
