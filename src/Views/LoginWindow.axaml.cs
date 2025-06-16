6daa215be6edc6f7be7683cf50083147fbcce166
Denis Nikitin <den221001@gmail.com>
2025-06-17 00:37:55 +0300

    feat: realize registration and login logic

--- src/Views/LoginWindow.axaml.cs
+++ src/Views/LoginWindow.axaml.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Data.Common;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Golden_votes.Entities;
 using Golden_votes.Utils;
 using Golden_votes.Utils.MessageBox;
 
@@ -15,7 +18,7 @@ public partial class LoginWindow : Window
     InitializeComponent();
     Settings.ConfigureWindow(this);
     this.LoginPanel.Width = this.Width / 3;
-    this.LoginPanel.Height = this.Height / 3.5;
+    this.LoginPanel.Height = this.Height / 2.8;
     this.LoginButton.Width = this.LoginPanel.Width;
   }
 
@@ -26,18 +29,47 @@ public partial class LoginWindow : Window
     if (string.IsNullOrEmpty(LoginTextBox.Text) ||
         string.IsNullOrEmpty(PasswordTextBox.Text))
     {
-      // InfoMessageBox.Show(this, "Information", "This is a message box in Avalonia!");
-      // return;
-
-      // var result = await VariantMessageBox.Show(this, "Confirmation", "Do you want to proceed?");
+      InfoMessageBox.Show(this, "Information", "Необходимо ввести логин и пароль!");
+      return;
     }
-    Encryption enc = new Encryption();
-    string hash_password = enc.Hash(LoginTextBox.Text + PasswordTextBox.Text);
-    string login = enc.Encrypt(LoginTextBox.Text);
-
-    AdminWindow win = new AdminWindow();
-    win.Show();
+    User user = new User(LoginTextBox.Text, PasswordTextBox.Text);
+    using (ApplicationContext db = new ApplicationContext())
+    {
+      if (db.Users.Where(u => u.ID == user.ID).FirstOrDefault() == null)
+      {
+        InfoMessageBox.Show(this, "Information", $"Пользователь {LoginTextBox.Text} отсутствует в системе!");
+        return;
+      }
+    }
+    Window childWindow = user.Role == User.UserRole.kBaseUser ? new AdminWindow() : // TODO: realize user window which take user object in constructor
+                                                                new AdminWindow();
+    childWindow.Show();
     this.Hide();
+  }
+
+  private async void OnRegisterClick(object? sender, RoutedEventArgs e)
+  {
+    // setup ip of server from json
+    // TODO: realize
+    if (string.IsNullOrEmpty(LoginTextBox.Text) ||
+        string.IsNullOrEmpty(PasswordTextBox.Text))
+    {
+      InfoMessageBox.Show(this, "Information", "Необходимо ввести логин и пароль!");
+      return;
+    }
+    User user = new User(LoginTextBox.Text, PasswordTextBox.Text);
+    using (ApplicationContext db = new ApplicationContext())
+    {
+      if (db.Users.Where(u => u.ID == user.ID).FirstOrDefault() != null)
+      {
+        InfoMessageBox.Show(this, "Information", $"Пользователь {LoginTextBox.Text} уже существует!");
+        return;
+      }
+      db.Users.Add(user);
+      db.SaveChanges();
+    }
+    LoginTextBox.Clear();
+    PasswordTextBox.Clear();
   }
 
   private async void SetupServerLocation(object? sender, RoutedEventArgs e)
