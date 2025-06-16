7041ceb40fa678fa6095c3c2d56e72a3462ffecd
Denis Nikitin <den221001@gmail.com>
2025-06-17 00:37:21 +0300

    feat + fix: rework entities dependencies + use hash + salt for ID because RSA use OAEP

--- src/Entities/User.cs
+++ src/Entities/User.cs
@@ -1,22 +1,32 @@
 using System;
+using System.Collections.Generic;
+using Golden_votes.Utils;
 
 namespace Golden_votes.Entities;
 
 public class User
 {
+  private const string id_salt = "golden_votes";
   public enum UserRole
   {
     kBaseUser,
     kAdmin
   };
+
+  public string ID { get; set; }
   public string Login { get; set; }
   public string Password { get; set; }
   public UserRole Role { get; set; }
 
+  public List<Question> Questions { get; set; }
+
+  public User() { }
   public User(string login, string password, UserRole role = UserRole.kBaseUser)
   {
-    Login = login;
-    Password = password;
+    Encryption enc = new Encryption();
+    ID = enc.Hash(login + id_salt);
+    Login = enc.Encrypt(login);
+    Password = enc.Hash(login + password);
     Role = role;
   }
 }
