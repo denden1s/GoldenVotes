48af5385375cb32996ea02eba2598b04bbdece0d
Denis Nikitin <den221001@gmail.com>
2025-06-21 19:49:30 +0300

    feat: add exit event for user form

--- src/Views/UserWindow.axaml.cs
+++ src/Views/UserWindow.axaml.cs
@@ -1,6 +1,8 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Golden_votes.Utils;
 
 namespace Golden_votes.Views;
 
@@ -9,5 +11,11 @@ public partial class UserWindow : Window
     public UserWindow()
     {
         InitializeComponent();
+        Settings.ConfigureWindow(this);
+        VotesList.Height = this.Height * 0.8;
+    }
+      private void OnWindowClosing(object? sender, WindowClosingEventArgs e)
+    {
+        Environment.Exit(0);
     }
 }
